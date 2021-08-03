6c94a85a8ec8f9846007c4169f0b747593dd09ed
Karen Aguirre <latingirl_2289@hotmail.com>
2021-08-03 09:44:17 +0200

    App piano en Angular 11 con Bootstrap 5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'App-Piano';
+
+  // Creamos el metodo
+  aplicarSonido(numero: number): void {
+    const audio = new Audio();
+    // Configuramos la ruta
+    audio.src = '../assets/sonidos/note' + numero + '.wav';
+    // Que cargue el audio
+    audio.load();
+    // Que lo ejecute
+    audio.play();
+  }
 }
